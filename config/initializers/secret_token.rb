9adaca27957cc14c667b7be0a6ead7db234e5072
Chad Glendenin <chad@glendenin.com>
2011-11-26 18:51:31 -0600

    init rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XmasList::Application.config.secret_token = 'da7266b5ded8af3262d4939d4f645b7f4feb9b779440aa1721a77d9b13acb595215ff1d300a3a3ef24b745e53ab6cadd24e1af7f1c0239183a610ce26d674372'
